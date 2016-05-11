7d403b6056f7ebe996c13d2bb7e98d5ba7483d0a
Allison Davis <a.rae.davis@gmail.com>
2016-05-11 16:13:24 -0700

    tests passing; fileContent function working

---
+++ parallel.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+
+//list of file names in files: beer.txt; cats.txt; yoda.txt;
+
+var fileContent =  function (file, callback){
+  var pathName = path.join(__dirname, file);
+
+  fs.readFile(pathName, (err, data) => {
+    if (err) return callback(err);
+    callback(null, data.toString());
+  });
+};
+
+exports.fileContent = fileContent;
--- test/parallel.test.js
+++ test/parallel.test.js
@@ -1,1 +1,22 @@
-const assert = ('assert');
+const assert = require('assert');
+//const fs = require('fs');
+//const path = require('path');
+const para = require('../parallel');
+
+
+describe('file reader', function(){
+
+  it('reads content', done => {
+    para.fileContent('cats.txt', (err, data) => {
+      assert.equal( data, 'meow\n');
+      done();
+    });
+
+  });
+
+
+  //it('retrieves first byte of file')
+
+  //it('returns an array in file order')
+
+});
