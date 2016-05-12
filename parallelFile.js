74d63525322b0fd76c03a4754fac1d2d3159ac9a
Johnny Luangphasy <johnny@jluangphasy.com>
2016-05-11 22:42:46 -0700

    Finished getFiles() and test for parallel file module

--- parallelFile.js
+++ parallelFile.js
@@ -2,22 +2,31 @@ const fs = require('fs');
 const path = require('path');
 
 const getFiles = (directory, callback) => {
-  fs.readdir('data', (error, files) => {
-    if (error) return callback();
+  fs.readdir(directory, (error, files) => {
+    if (error) return callback(error);
 
-    const results = [];
-    const filesCount = files.length;
+    const fileResults = [];
+    var filesCount = files.length;
 
-    files.forEach((file) => {
-      const filePath = path.join(directory, file);
-
-      fs.readFile(filePath, 'utf-8', (error, fileText) => {
+    const readFile = filePath => {
+      fs.readFile(filePath, 'utf-8', (error, fileContent) => {
         if (error) return callback();
 
-        results.push(fileText.split(' ')[0]);
+        const fileBasename = path.basename(filePath);
+        const fileFirstWord = fileContent.split(' ')[0];
 
-        if (!--filesCount) callback(null, results);
+        fileResults.push(`${fileBasename} ${fileFirstWord}`);
+
+        if (!--filesCount) callback(null, fileResults);
       });
+    };
+
+    files.forEach(file => {
+      const filePath = path.join(directory, file);
+
+      readFile(filePath);
     });
   });
 };
+
+module.exports = getFiles;
--- tests/parallelFile.test.js
+++ tests/parallelFile.test.js
@@ -1,4 +1,32 @@
 const assert = require('assert');
-const fs = require('fs');
 const path = require('path');
-const parallelFile = require('../parallelFile');
+const getFiles = require('../parallelFile');
+
+describe('get files in directory', _ => {
+  it('should return an array of each file basename with first word in the file', done => {
+    const directory = path.join(__dirname, '../files');
+
+    getFiles(directory, (error, files) => {
+      if (error) return done(error);
+
+      assert.ok(files.indexOf('file-01.txt Bacon') >= 0, 'file-01.txt with the first word "Bacon" does not exist');
+      assert.ok(files.indexOf('file-02.txt Lorem') >= 0, 'file-02.txt with the first word "Lorem" does not exist');
+      assert.ok(files.indexOf('file-03.html <!DOCTYPE') >= 0, 'file-03.html with the first word "<!DOCTYPE" does not exist');
+      assert.ok(files.indexOf('file-04.html <!DOCTYPE') >= 0, 'file-04.html with the first word "<!DOCTYPE" does not exist');
+      assert.ok(files.indexOf('file-05.md #') >= 0, 'file-05.md with the first word "#" does not exist');
+      assert.ok(files.indexOf('file-06.md #') >= 0, 'file-06.md with the first word "#" does not exist');
+
+      done();
+    });
+  });
+
+  it('should not return an array of each file basename with first word in the file', done => {
+    const directory = path.join(__dirname, '../data');
+
+    getFiles(directory, (error) => {
+      assert.ifError(error.Error);
+
+      done();
+    });
+  });
+});
